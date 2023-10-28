b2e71ef04734a983aa3f24708f0b79fc35494fed
gigib <glendaandrews77@gmail.com>
2023-10-28 10:26:16 -0700

    url paths

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("post/", include("posts.urls")),
+    path("", include("pages.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
      path("accounts/", include("accounts.urls")),
 ]
